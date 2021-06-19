ff68bc810d7b0c8abb9f6728ad307abfe7a548d5
houpei <houshoupei@126.com>
2021-06-19 21:59:45 +0800

    change config for main net

--- sendmigratedidtx.js
+++ sendmigratedidtx.js
@@ -1,5 +1,5 @@
 Web3 = require("web3");
-web3 = new Web3("http://127.0.0.1:1111");
+web3 = new Web3("http://127.0.0.1:20646");
 //var Tx = require('ethereumjs-tx');
 //var Transaction = require('ethereumjs-tx').Transaction;
 var Tx = require('ethereumjs-tx').Transaction;
@@ -38,10 +38,11 @@ contract = new web3.eth.Contract([
     }
 ]);
 //did合约地址
-contract.options.address = "0x491bC043672B9286fA02FA7e0d6A3E5A0384A31A";
+contract.options.address = "0x46E5936a9bAA167b3368F4197eDce746A66f7a7a";
 
 //账号
-acc = web3.eth.accounts.decrypt({"address":"53781e106a2e3378083bdcede1874e5c2a7225f8","crypto":{"cipher":"aes-128-ctr","ciphertext":"bc53c1fcd6e31a6392ddc1777157ae961e636c202ed60fb5dda77244c5c4b6ff","cipherparams":{"iv":"c5d1a7d86d0685aa4542d58c27ae7eb4"},"kdf":"scrypt","kdfparams":{"dklen":32,"n":262144,"p":1,"r":8,"salt":"409429444dabb5664ba1314c93f0e1d7a1e994a307e7b43d3f6cc95850fbfa9f"},"mac":"4c37821c90d35118182c2d4a51356186482662bb945f0fcd33d3836749fe59c0"},"id":"39e7770e-4bc6-42f3-aa6a-c0ae7756b607","version":3}, "123");
+//acc = web3.eth.accounts.decrypt({"address":"53781e106a2e3378083bdcede1874e5c2a7225f8","crypto":{"cipher":"aes-128-ctr","ciphertext":"bc53c1fcd6e31a6392ddc1777157ae961e636c202ed60fb5dda77244c5c4b6ff","cipherparams":{"iv":"c5d1a7d86d0685aa4542d58c27ae7eb4"},"kdf":"scrypt","kdfparams":{"dklen":32,"n":262144,"p":1,"r":8,"salt":"409429444dabb5664ba1314c93f0e1d7a1e994a307e7b43d3f6cc95850fbfa9f"},"mac":"4c37821c90d35118182c2d4a51356186482662bb945f0fcd33d3836749fe59c0"},"id":"39e7770e-4bc6-42f3-aa6a-c0ae7756b607","version":3}, "123");
+acc = web3.eth.accounts.decrypt(account, "did contract account password");
 
 //var myContract = MyContract.at("0x491bC043672B9286fA02FA7e0d6A3E5A0384A31A");
 // contract.defaults({
